628d7449fdb4ccd38fb3a9d55c369c23b4d92729
Joe O'Malley <joe.ryan.omalley@gmail.com>
2026-01-30 11:42:44 -0800

    Update database connection for Render

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,9 @@ require('dotenv').config();
 
 let sequelize;
 
-if (process.env.DB_URL) {
+if (process.env.DATABASE_URL) {
+  sequelize = new Sequelize(process.env.DATABASE_URL);
+} else if (process.env.DB_URL) {
   sequelize = new Sequelize(process.env.DB_URL);
 } else {
   sequelize = new Sequelize(
